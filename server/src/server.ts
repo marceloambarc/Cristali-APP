ff88e28be53f50c7a2370e0627e717e236a88a39
marceloambarc <marcelo.ambarc@gmail.com>
2021-07-15 17:46:09 -0300

    15/07 - 14:45

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,16 @@
-const a: number = 5;
+import express from 'express'
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-console.log(a);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+  res.send('Hello World!');
+});
+
+app.listen(3333,() => {
+  console.log('Rodando.')
+});
